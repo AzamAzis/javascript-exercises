05ce5c99f5e79796b5eee1f39a0f7966e0253ed8
Azam Azis <aziz76218@gmail.com>
2026-03-31 21:15:25 +0700

    06_repeatString is done

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = (strings, number) => {
+	let words = "";
 
+	if (number >= 0) {
+		for (let i = 0; i < number; i++) {
+			words += strings;
+		}
+	} else {
+		words = "ERROR";
+	}
+
+	return words;
 };
 
 // Do not edit below this line
