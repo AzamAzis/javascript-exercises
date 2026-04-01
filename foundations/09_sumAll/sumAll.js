474224835391c160bbad06b8afdc117cb0d2a411
Azam Azis <aziz76218@gmail.com>
2026-04-01 21:14:22 +0700

    09_sumAll is finished

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,29 @@
-const sumAll = function() {
+const sumAll = (num1, num2) => {
+	const array = [];
+	const min = Math.min(num1, num2);
+	const max = Math.max(num1, num2);
 
+	for (let i = min; i <= max; i++) {
+		array.push(i);
+	}
+
+	if (
+		min < 0 ||
+		min !== parseInt(min) ||
+		max !== parseInt(max) ||
+		num1 === String(num1) ||
+		num2 === String(num2) ||
+		min !== Number(min) ||
+		max !== Number(max)
+	) {
+		return "ERROR";
+	}
+
+	const sum = array.reduce((accumulator, item) => {
+		return accumulator + item;
+	});
+
+	return sum;
 };
 
 // Do not edit below this line
