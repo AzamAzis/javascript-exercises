bb2a765175cf8a1b1b2a8ca9afcd82be58b0f1ab
Azam Azis <aziz76218@gmail.com>
2026-04-01 15:26:38 +0700

    08_removeFromArray is finished

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = (arr, ...number) => {
+	const filtered = arr.filter((item) => {
+		return !number.includes(item);
+	});
+
+	return filtered;
 };
 
 // Do not edit below this line
