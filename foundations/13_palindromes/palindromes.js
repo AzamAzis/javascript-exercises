2761171a63bc45de108ef410b414dc69adf0d037
Azam Azis <aziz76218@gmail.com>
2026-05-16 19:48:25 +0700

    finished

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
+const palindromes = (words) => {
+	const input = words.toLowerCase().replace(/[^a-z0-9]/gi, "");
+	const output = input.split("").reverse().join("");
 
+	return input === output;
 };
 
 // Do not edit below this line
