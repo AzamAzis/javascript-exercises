1cc7451025d7792e7e5e731b56828c7971860700
Azam Azis <aziz76218@gmail.com>
2026-05-17 13:39:28 +0700

    done

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,26 @@
-const fibonacci = function() {
+const fibonacci = function (number) {
+  let num;
 
+  if (typeof number !== "number") {
+    num = parseInt(number, 10);
+  } else {
+    num = number;
+  }
+
+  if (number < 0) return "OOPS";
+  if (number == 0) return 0;
+  if (number == 1) return 1;
+
+  let first = 1;
+  let second = 0;
+
+  for (let i = 2; i <= number; i++) {
+    let current = first + second;
+    second = first;
+    first = current;
+  }
+
+  return first;
 };
 
 // Do not edit below this line
