2d39fe94842063f9831cc735daa9632d5e9d44e2
Azam Azis <aziz76218@gmail.com>
2026-05-17 16:02:48 +0700

    done

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+	const groupOfPeople = people.reduce((array, object) => {
+		const live = !object.yearOfDeath
+			? new Date().getFullYear()
+			: object.yearOfDeath;
 
+		const newObject = {
+			name: object.name,
+			age: live - object.yearOfBirth,
+		};
+
+		array.push(newObject);
+
+		const sortPeople = array.sort((a, b) => {
+			return b.age - a.age;
+		});
+
+		return sortPeople;
+	}, []);
+
+	return groupOfPeople[0];
 };
 
 // Do not edit below this line
